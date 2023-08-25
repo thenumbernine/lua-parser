de5d393583b32072194088ece4ac44038ab5fe4c
Christopher Moore <christopher.e.moore@gmail.com>
2023-08-25 13:25:47 +0700

    thought a lot about what conditions to upgrade language from 5.1 to 5.2, considered the parsability of 'goto=1', ended up sticking with the original test.

--- tests/minify_tests.lua
+++ tests/minify_tests.lua
@@ -2,7 +2,29 @@
 --taken from the tests at https://github.com/stravant/LuaMinify
 local parser = require 'parser'
 local version = _VERSION:match'^Lua (.*)$'
-if version == '5.1' and jit then version = '5.2' end
+
+-- This is due to my own luajit compiling with 5.2 compat...
+-- But this begs the question, are there any flags testable when compiling luajit with LUAJIT_ENABLE_LUA52COMPAT ?
+-- https://love2d.org/forums/viewtopic.php?t=83187&sid=d189c94812b6f7ed4ebf8b2253b39828
+-- Seems you can test 'goto' for this
+-- But load vs loadstring ... in 5.1 load() expects a function and loadstring() expects a string
+-- While in 5.2 (or luajit + 5.2 compat?) load() accepts strings as well.
+-- Which means I'll have to use loadstring() in the validation test ...
+-- Mind you in Lua 5.3 that loadstring() no longer exists and this will produce an error.
+if version == '5.1' then
+	--[[
+	if not pcall(loadstring'local goto=1') then
+		version = '5.2'
+	end
+	--]]
+	-- but then again, when it comes to luajit, the first test passes/fails between 2.0.5 and 2.1.0-beta3 (I think with/without LUAJIT_ENABLE_LUA52COMPAT...)
+	-- but ::a:: goto a works in both my versions of LuaJIT.
+	-- so instead I'll just upgrade all luajit versions:
+	-- [[
+	if jit then version = '5.2' end
+	--]]
+end
+
 print(version)
 for line in io.lines'minify_tests.txt' do
 	local expected = not line:match('FAIL_'..version)
