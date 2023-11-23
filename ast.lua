bcb2af0653392966d65b1233fe818918140a7402
Christopher Moore <christopher.e.moore@gmail.com>
2023-11-23 16:40:45 +0700

    string.defaultConcat -> string.concat

--- ast.lua
+++ ast.lua
@@ -255,7 +255,7 @@ local function nodeclass(contents, parent)
 	-- TODO put in root-most ast class
 	newclass.__tostring = nodeToString
 
-	newclass.__concat = string.defaultConcat
+	newclass.__concat = string.concat
 
 	allclasses:insert(newclass)
 	return newclass
