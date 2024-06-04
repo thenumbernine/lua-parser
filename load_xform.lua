acb3938caf956227f1cf49aa28639c883ca7a8ea
thenumbernine <christopher.e.moore@gmail.com>
2024-06-04 09:13:16 +0800

    added parser.load_xform to replace parser.require for shim ast tree transforms that operate now on load, loadfile, and require

---
+++ load_xform.lua
@@ -0,0 +1,31 @@
+--[[
+parser.require only overrides the require statement, and is deprecated. 
+parser.load_xorm modifies the load(), and loadfile() and require() to go along with it.
+--]]
+local LuaParser = require 'parser.lua.parser'
+
+local callbacks = setmetatable({}, {__index=table})
+
+require 'ext.load'.xforms:insert(function(d, source)
+	local parser
+	local result, code = xpcall(function()
+		parser = LuaParser()
+		parser:setData(d, source)
+		local tree = parser.tree
+		for _,cb in ipairs(callbacks) do
+			cb(tree)
+		end
+		return tostring(tree)
+	end, function(err)
+		return tostring(source)
+			-- TODO move this into LuaParser itself's error generation
+			..' at '..parser.t:getpos()..'\n'
+			..err..'\n'
+			..debug.traceback()
+	end)
+	if not result then error(code) end
+	return code
+
+end)
+
+return callbacks
